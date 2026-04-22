d7baecf40e059af713662e48fe789492346f05bb
Robert Chirinos <robert.chirinos@pucp.edu.pe>
2026-04-22 15:18:49 -0500

    Update nvim-treesitter to main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,16 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    event = "BufEnter *.*",
+    event = { "BufReadPre *.*", "BufNewFile *.*" },
     build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        ensure_installed = { "java", "lua", "vim", "vimdoc", "query" },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "*",
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+        end,
       })
-    end
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
